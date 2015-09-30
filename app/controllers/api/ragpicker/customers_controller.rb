2ac3f53986b21349195d5d2a167df4d248a4912e
Duc Le <ducle@digifutura.com>
2015-09-30 10:34:09 +0700

    add multiple customers

--- app/controllers/api/ragpicker/customers_controller.rb
+++ app/controllers/api/ragpicker/customers_controller.rb
@@ -2,7 +2,7 @@ class Api::Ragpicker::CustomersController < Api::ApiController
 
   def add_customers
     customers = []
-    params[:customers].each do |cus|
+    params[:customers].each do |key, cus|
       customer = Customer.find_or_create_by name: cus[:name], phone_number: cus[:phone_number]
       customers << customer if customer.errors.blank?
     end
