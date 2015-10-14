154fc18bc62abb0d3daeeac82da7d96a7d288762
Tin <tinle1201@gmail.com>
2015-10-14 13:45:37 +0700

    not validate customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ActiveRecord::Base
   validates :name, presence: true
-  validates :phone_number, uniqueness: true, presence: true
+  validates :phone_number, presence: true
 
   has_many :pick_ups
 end
