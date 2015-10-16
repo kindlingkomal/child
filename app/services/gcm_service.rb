550c23305e572aa707e3ddda697a97e1c456326c
Tin <tinle1201@gmail.com>
2015-10-16 09:20:06 +0700

    send sms password

--- app/services/gcm_service.rb
+++ app/services/gcm_service.rb
@@ -2,7 +2,7 @@ require 'gcm'
 class GcmService
   def initialize user
     @current_user = user
-    @gcm = GCM.new('AIzaSyCDcjxhc_7sTLGC2u4oRUwvDxsgecyhjpk')
+    @gcm = GCM.new(ENV['GCM_SECRET_KEY']||'AIzaSyCDcjxhc_7sTLGC2u4oRUwvDxsgecyhjpk')
   end
 
   def push_to_registration_ids(reg_ids, opts={})
@@ -10,10 +10,6 @@ class GcmService
     return false if reg_ids.blank? || opts[:data].blank?
     reg_ids = reg_ids.compact.uniq
     return false if reg_ids.blank?
-    # reg_ids = [
-    #   # 'e2K_J_kYVBo:APA91bEqaHkMW8M23UT23A_J4QzXJJP0-auUlKNtsF7wulKrnxIwxb1MFygV19d3fVwppLmMr2hyp7mNpgJY2s_IAmc6vBXQG4k_vdvTnhKW5fzwXus-kSPiQNPE_kMub1bsD-SJY7YC',
-    #   'fgA_dT7hKKA:APA91bFlAzP6brJ9Gs8brMxnLZXnzzIqHWO2oguwza4DVVG8VHkQPWj0Ti0v6NnLN7RaLaLSLhzCPIhunxJvLGyOI5bbgMRNVQ_vLE0sHUfkF__3m4ctQO9IJLzvcQTkGFvIKftzLLpM'
-    # ]
     options = opts.merge({
       # collapse_key: "demo",
       delay_while_idle: true,
---
+++ app/services/sms_service.rb
@@ -0,0 +1,19 @@
+require 'rest-client'
+class SmsService
+
+  def self.send(to, message)
+    raw = RestClient.post ENV['SMS_API_URL'], to: to, message: message
+    JSON.parse raw
+  end
+
+  def self.status(json)
+    json['data'].nil? ? false : json['data']['0']['status'] == 'AWAITED-DLR'
+  end
+
+  def self.send_otp(to, otp, country='IN')
+    message = "Your STV password: #{otp}"
+    sms = send(to, message)
+    status(sms)
+  end
+
+end
