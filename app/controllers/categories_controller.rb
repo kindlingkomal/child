335f38282c9797ae8bc496d457ae542d2db2cd15
Bao <bao@digifutura.com>
2016-01-04 17:40:41 +0700

    show featured categories on home page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   layout 'customer'
 
   def index
-    @categories = Category.order(:name)
+    @categories = Category.order(:status, :name)
     @selected_category_ids = session[:selected_category_ids] || []
   end
 end
