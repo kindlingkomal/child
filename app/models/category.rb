c68ffafbd5cc7c705b8e3d6af6ffabe981cc4eea
Bao <bao@digifutura.com>
2016-01-07 15:13:35 +0700

    fix: user is able to create duplicate category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,8 @@
 class Category < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+  strip_attributes :only => [:name]
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive:  false }
   validates :price, presence: true, :numericality => {:greater_than => 0}
 
   has_many :line_items, dependent: :destroy
