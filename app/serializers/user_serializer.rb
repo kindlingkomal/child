3536526d9035fb9b2a9eebd9502a7e8bedcaf70c
Bao <bao@digifutura.com>
2016-01-11 14:03:35 +0700

    add yard_number, is_yard to UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :full_name, :phone_number, :created_at, :authentication_token,
-    :avatar, :gender, :address, :city, :notified, :pincode, :lat, :lon
+    :avatar, :gender, :address, :city, :notified, :pincode, :lat, :lon,
+    :yard_number, :is_yard
 
   def authentication_token
     show_token = meta.try(:[], :show_token)
