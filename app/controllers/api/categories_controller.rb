c833682602d7dcfc0256a60d7c640eb870ebf605
Bao <bao@digifutura.com>
2016-01-13 10:29:58 +0700

    list categories

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -3,6 +3,9 @@ class Api::CategoriesController < Api::ApiController
   skip_before_action :authenticate_user!
 
   def index
-    render json: Category.order(name: :asc)
+    term = params[:term].presence
+    categories = Category.order(name: :asc)
+    categories = categories.where("lower(name) like :q", q: "%#{term.downcase}%") if term
+    render json:categories
   end
 end
