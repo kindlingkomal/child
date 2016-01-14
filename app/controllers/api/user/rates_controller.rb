7f4970157b6204e05bac7ebd5ed20b03021188b8
Bao <bao@digifutura.com>
2016-01-14 16:40:18 +0700

    fix: user is allowed to rate multiple times for the same order

--- app/controllers/api/user/rates_controller.rb
+++ app/controllers/api/user/rates_controller.rb
@@ -5,6 +5,7 @@ class Api::User::RatesController < Api::UserController
     @user = User.find(params[:user_id])
     pick_up_id = params[:pick_up_id].presence
     pickup = PickUp.find_by(id: pick_up_id)
+    render json: {error: {code: 20001, msg: "This order was rated."}}, status: 405 and return if pickup.has_ratings
     if pickup && pickup.status? && pickup.status != 'pending' &&
     @user.rate_for_pickup(pick_up_id, params[:score].to_f, @current_user, dimension)
       @current_rate = @user.reload.rates(dimension).where(rater_id: @current_user.id).
