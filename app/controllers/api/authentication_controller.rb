4c9b51b7d30e812e7802ea377b6bd3a57b7c7598
Bao <bao@digifutura.com>
2016-01-14 13:57:10 +0700

    change error message 'role invalid'

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -43,7 +43,7 @@ class Api::AuthenticationController < Api::ApiController
 protected
   def login_user(user)
     unless user.user?
-      render json: {error: {code: "20001", msg: "role invalid"}}, status: 403 and return
+      render json: {error: {code: "20001", msg: "Phone number or password is incorrect"}}, status: 403 and return
     end
     user.last_sign_in_at = Time.now
     user.invalidate_authentication_token
