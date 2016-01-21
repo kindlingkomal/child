31700dfdcfadb9aa01bec39e0139178f8314e4c7
Bao <bao@digifutura.com>
2016-01-21 10:00:42 +0700

    add pick_time to User::PickupSerializer

--- app/serializers/user/pickup_serializer.rb
+++ app/serializers/user/pickup_serializer.rb
@@ -1,10 +1,12 @@
 class User::PickupSerializer < PickupSerializer
   attributes :id, :address, :city, :pincode, :lat, :lon, :landmark, :subscription,
-    :start_time, :end_time, :category_ids, :status, :total, :has_ratings
+    :start_time, :end_time, :pick_time, :category_ids, :status, :total, :has_ratings
 
   # belongs_to :user, serializer: UserCommonSerializer
   belongs_to :ragpicker, serializer: UserCommonSerializer
   has_many :line_items
 
-
+  def pick_time
+    object.pick_time
+  end
 end
