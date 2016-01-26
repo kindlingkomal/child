9db85c2a4f1df96884619cb0fcb058878a91b6b9
Bao <bao@digifutura.com>
2016-01-26 23:36:52 +0700

    add validation to model Subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,38 @@
 class Subscription < ActiveRecord::Base
+  enum frequency: [:no, :daily, :weekly, :monthly]
+
+  validates :address, :city, :start_time, :end_time, :category_set, presence: true
+  validates :user, presence: true
+  validates :date, presence: true
+  validates :time_slot_id, presence: true, uniqueness: {scope: [:user_id, :date]}
+  validate :check_start_time
+
   belongs_to :user
   belongs_to :time_slot
+
+  before_validation :set_default_frequency, :set_time
+
+private
+
+  def set_default_frequency
+    self.frequency ||= :no
+  end
+
+  def set_time
+    if date.present? && time_slot_id.present?
+      start_date = date.to_date
+      timeslot = TimeSlot.find time_slot_id
+      self.start_time = start_date + timeslot.start_hour.seconds
+      self.end_time = start_date + timeslot.end_hour.seconds
+    else
+      self.date = nil
+      self.time_slot_id = nil
+    end
+  end
+
+  def check_start_time
+    if start_time < Time.zone.now
+      errors.add(:start_time, "Start time cannot be in the past")
+    end
+  end
 end
