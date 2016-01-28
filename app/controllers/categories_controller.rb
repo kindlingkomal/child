01ec7a983486186a1ae2d0b29f588ccddd1778d1
Bao <bao@digifutura.com>
2016-01-28 13:02:16 +0700

    on rate card page, categories should sort in ascending order

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   layout 'customer'
 
   def index
-    @categories = Category.order(:status, :name)
+    @categories = Category.order(:name)
     @selected_category_ids = session[:selected_category_ids] || []
   end
 end
