de4b2fbf9b74f9e472714ff11c1a1da5971780b6
Bao <bao@digifutura.com>
2016-01-30 14:36:35 +0700

    redirect to the root page when the user is unauthorized

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   enum gender: [:male, :female]
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => {phone_number: true, login: false}
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => {phone_number: true}
   ratyrate_rateable 'customer', 'ragpicker' #, 'experience'
   ratyrate_rater
 
