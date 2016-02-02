6ccb5fc79233ed8f196db84ade0c330410b19966
Bao <bao@digifutura.com>
2016-02-02 18:23:33 +0700

    rating customer: return error code 20001, "User not registered" if customer is not found

--- app/controllers/api/picker/rates_controller.rb
+++ app/controllers/api/picker/rates_controller.rb
@@ -2,8 +2,10 @@ class Api::Picker::RatesController < Api::PickerController
 
   def create
     dimension = 'customer'
-    @user = User.find(params[:user_id])
-    if @user.rate(params[:score].to_f, @current_user, dimension)
+    @user = User.find_by(id: params[:user_id])
+    if @user.nil?
+      render json: {error: {code: 20001, msg: "User not registered"}}, status: 405
+    elsif @user.rate(params[:score].to_f, @current_user, dimension)
       @current_rate = @user.reload.rates(dimension).where(rater_id: @current_user.id).take
       @current_rate.update(comment: params[:comment])
       render json: {success: true}
